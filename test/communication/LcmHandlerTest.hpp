9f266d8b72c6d08dfd6d9cd8c0f200f0c56e6bf6
Wsine <weizhy3@hotmail.com>
2017-02-03 16:38:38 +0800

    compile test code successfully

--- test/communication/LcmHandlerTest.hpp
+++ test/communication/LcmHandlerTest.hpp
@@ -1,10 +1,11 @@
 #include <boost/test/unit_test.hpp>
-#include "LcmHandler.h"
+#include "communication/LcmHandler.h"
 
 BOOST_AUTO_TEST_SUITE(LcmHandlerTestSuite)
 
-BOOST_FIXTURE_TEST_CASE(PassTest, LcmHandler) {
-    BOOST_CHECK_EQUAL(540, calcBrakeAim(50, 10));
+BOOST_AUTO_TEST_CASE(PassTest) {
+    LcmHandler handler;
+    BOOST_CHECK_EQUAL(540, handler.calcBrakeAim(50, 10));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
